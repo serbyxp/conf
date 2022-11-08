1c463ad13898fa174d95149cd904da98891ca775
Serby Xp <serbyxp@gmail.com>
2022-11-07 23:58:03 -0500

    1

---
+++ LoRa/SX1302/reset_lgw.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+SX1302_RESET_PIN=17
+
+WAIT_GPIO() {
+    sleep 0.1
+}
+
+init() {
+    # setup GPIOs
+    echo "$SX1302_RESET_PIN" > /sys/class/gpio/export; WAIT_GPIO
+
+    # set GPIOs as output
+    echo "out" > /sys/class/gpio/gpio$SX1302_RESET_PIN/direction; WAIT_GPIO
+}
+
+reset() {
+    echo "CoreCell reset through GPIO$SX1302_RESET_PIN..."
+
+
+    echo "1" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
+    echo "0" > /sys/class/gpio/gpio$SX1302_RESET_PIN/value; WAIT_GPIO
+}
+
+term() {
+    # cleanup all GPIOs
+    if [ -d /sys/class/gpio/gpio$SX1302_RESET_PIN ]
+    then
+        echo "$SX1302_RESET_PIN" > /sys/class/gpio/unexport; WAIT_GPIO
+    fi
+}
+
+case "$1" in
+    start)
+    term # just in case
+    init
+    reset
+    ;;
+    stop)
+    reset
+    term
+    ;;
+    *)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+esac
+
+exit 0
